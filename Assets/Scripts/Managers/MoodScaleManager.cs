99ada940b6dfa18ce5610648531ca8e67bcc0980
Ciara Mckenzie <ciara.mck23@gmail.com>
2026-08-05 11:28:00 +1000

    mood scale

---
+++ Assets/Scripts/Managers/MoodScaleManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoodScaleManager : MonoBehaviour
+{
+    public Slider moodSlider;
+    public Slider calmSlider;
+    public Slider attentionSlider;
+    public GameObject question3;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(ExperimentManager.Instance.currentState == ExperimentManager.ExperimentState.PreMood)
+        {
+            question3.SetActive(false);
+        }
+        else if (ExperimentManager.Instance.currentState == ExperimentManager.ExperimentState.PostMood)
+        {
+            question3.SetActive(true);
+        }
+    }
+
+    public void Submit()
+    {
+
+        float mood = moodSlider.value;
+        float calm = calmSlider.value;
+        float attention = attentionSlider.value;
+        ExperimentManager.Instance.ContinueExperiment();
+    }
+
+}
